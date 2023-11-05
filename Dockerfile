63c0c9fde6f8cc62766294d09d5f86a8a5fabc08
rdragan <rdragan@student.42heilbronn.de>
2023-11-05 11:02:47 +0100

    #5 Create another Dockerfile and run_in_docker.sh

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,9 @@
 #    By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/04 09:32:14 by rdragan           #+#    #+#              #
-#    Updated: 2023/11/05 10:12:52 by rdragan          ###   ########.fr        #
+#    Updated: 2023/11/05 10:51:29 by rdragan          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 FROM alpine:latest
-COPY . /app
 RUN apk add clang g++ make valgrind
-CMD cd app && make re && ./miniRT
--- run_in_docker.sh
+++ run_in_docker.sh
@@ -6,30 +6,16 @@
 #    By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/04 09:38:43 by rdragan           #+#    #+#              #
-#    Updated: 2023/11/05 10:38:49 by rdragan          ###   ########.fr        #
+#    Updated: 2023/11/05 11:01:15 by rdragan          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
-# check weather the image exists or not and connect to it
-if [ -z "$(docker images | grep mini_rt_env)" ]; then
-	# build the container
-	docker build -t mini_rt_env .
-	# run the container
-	docker run mini_rt_env
-	for i in $(docker ps -a | grep "mini_rt_env" | awk '{print $1}');do echo Removed: && docker rm $i; done
+if [ -z "$(docker images | grep "mini_rt_env"| head -1)" ]; then
+	docker build -t "mini_rt_env" .
+	docker run -it -v $PWD:/app -w /app mini_rt_env
 else
-	# run the container
-	docker run mini_rt_env
-	# remove containers
-	for i in $(docker ps -a | grep "mini_rt_env" | awk '{print $1}');do echo Removed: && docker rm $i; done
-	
-	echo "Do you want to remove image?[Y/N]"
-	read option
-	if [ $option == "Y" ]; then
-		docker rmi $(docker images | grep mini_rt_env | awk '{print $3}')
-	elif [ $option == "Y" ]; then
-		docker rmi $(docker images | grep mini_rt_env | awk '{print $3}')
-	fi
+	docker run -it -v $PWD:/app -w /app mini_rt_env
 fi
+docker rm $(docker ps -a | grep mini_rt_env | head -1 | awk '{print $1}')
