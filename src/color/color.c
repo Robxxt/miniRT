880257fc4b6749e61df45537bae4f7e6653bb3a4
TianhengQin <tianhengchin@gmail.com>
2023-12-11 21:35:46 +0100

    color

--- src/color/color.c
+++ src/color/color.c
@@ -1,3 +1,50 @@
 #include "../../includes/miniRT.h"
 
+unsigned int	get_color(t_color *rgb)
+{
+	unsigned int	c;
 
+	// if (rgb->bright == 0)
+	// 	return (0);
+	c = 0;
+	if (rgb->b > 255)
+		c = c + 255;
+	else
+		c = c + rgb->b;
+	if (rgb->g > 255)
+		c = c + 0x00000100 * 255;
+	else
+		c = c + 0x00000100 * rgb->g;
+	if (rgb->r > 255)
+		c = c + 0x00010000 * 255;
+	else
+		c = c + 0x00010000 * rgb->r;
+	if (rgb->bright > 255)
+		c = c + 0x01000000 * 255;
+	else
+		c = c + 0x01000000 * rgb->bright;
+	return (c);
+}
+
+t_color	color_mix(t_color *a, t_color *b)
+{
+	t_color	re;
+
+	re.b = a->b + b->b;
+	re.g = a->g + b->g;
+	re.r = a->r + b->r;
+	re.bright = a->bright + b->bright;
+	if (re.bright > 255)
+		re.bright = 255;
+	return (re);
+}
+
+t_color	color_reflect(t_color *a, t_color *b)
+{
+	t_color	re;
+
+	re.b = a->b * b->b / 255;
+	re.g = a->g * b->g / 255;
+	re.r = a->r * b->r / 255;
+	return (re);
+}
