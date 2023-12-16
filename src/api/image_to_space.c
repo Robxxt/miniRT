f1505e0abab902609a6e6677f8c93ff66bc9cad1
rdragan <rdragan@student.42heilbronn.de>
2023-12-16 20:28:42 +0100

    made it somehow to work

--- src/api/image_to_space.c
+++ src/api/image_to_space.c
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 19:20:41 by rdragan           #+#    #+#             */
-/*   Updated: 2023/12/16 19:25:51 by rdragan          ###   ########.fr       */
+/*   Updated: 2023/12/16 20:25:33 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	image_to_space(t_image p1, t_space* p2)
 	ambient_to_ambt(p1.ambient, &(p2->ambient));
 	camara_to_cmr(p1.camara, &(p2->cmr));
 	light_to_lit(p1.light, &(p2->lit[0]));
+	p2->lit[1].pos.x = NAN;
 	sphere_to_sp(p1.sphere, &(p2->sp));
 	plane_to_panel(p1.plane, &(p2->pl));
 	cylinder_to_cylind(p1.cylinder, &(p2->cylind));
