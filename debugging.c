2248bd5420b6c216803c30932c4b0f668b3fa2ae
rdragan <rdragan@student.42heilbronn.de>
2023-12-16 19:57:36 +0100

    print space

--- debugging.c
+++ debugging.c
@@ -6,7 +6,7 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 15:32:43 by rdragan           #+#    #+#             */
-/*   Updated: 2023/12/16 19:50:15 by rdragan          ###   ########.fr       */
+/*   Updated: 2023/12/16 19:57:19 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	print_vector(t_vector vector, char *s)
 
 void	print_panel(t_panel panel)
 {
-	printf("PANEL\n");
+	printf("\tPANEL\n");
 	printf("pos: %f,%f,%f\n", panel.pos.x, panel.pos.y, panel.pos.z);
 	printf("nv: %f,%f,%f\n", panel.nv.x, panel.nv.y, panel.nv.z);
 	printf("rgb: %d,%d,%d\n", panel.rgb.r, panel.rgb.g, panel.rgb.b);
@@ -35,13 +35,13 @@ void	print_panel(t_panel panel)
 
 void	print_ambient(t_ambt a)
 {
-	printf("AMBIENT\n");
+	printf("\tAMBIENT\n");
 	print_color(a.rgb);
 }
 
 void	print_camara(t_cmr c)
 {
-	printf("CAMARA\n");
+	printf("\tCAMARA\n");
 	print_vector(c.nv, "nv");
 	print_vector(c.pos, "pos");
 	printf("fv: %d\n", c.fv);
@@ -49,17 +49,26 @@ void	print_camara(t_cmr c)
 
 void	print_light(t_lit c)
 {
-	printf("LIGHT\n");
+	printf("\tLIGHT\n");
 	print_vector(c.pos, "pos");
 	print_color(c.rgb);
 }
 
 void	print_sp(t_sp sp)
 {
-	printf("sp\n");
+	printf("\tSPHERE\n");
 	printf("pos: %f,%f,%f\n", sp.pos.x, sp.pos.y, sp.pos.z);
 	printf("rgb: %d,%d,%d\n", sp.rgb.r, sp.rgb.g, sp.rgb.b);
 	printf("texture: %c\tradii: %f\n", sp.texture, sp.radii);
+}
+
+void	print_cy(t_cylind c)
+{
+	printf("\tCYLIND\n");
+	print_vector(c.pos, "pos");
+	print_vector(c.nv, "nv");
+	print_color(c.rgb);
+	printf("texture: %c\tradii: %f\theight: %f\n", c.texture, c.radii, c.height);
 }
 
 void	print_space(t_space s)
@@ -70,5 +79,10 @@ void	print_space(t_space s)
 	print_ambient(s.ambient);
 	print_camara(s.cmr);
 	print_light(s.lit[0]);
-	print_sp(s.sp);
+	if(s.sp.exists)
+		print_sp(s.sp);
+	if(s.pl.exists)
+		print_panel(s.pl);
+	if(s.cylind.exists)
+		print_cy(s.cylind);
 }
